140f3a04811146adaf26f847e6f0afd5c3b16490
Joshua Salcedo <71166003+JuswaOof@users.noreply.github.com>
2022-06-04 21:16:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const currentDate = new Date();
-
+const dateCountdown = new Date(Jan 1, 2023);
 
 console.log(currentDate.getTime());
+console.log(dateCountdown.getTime());
 
 document.getElementById("date").innerHTML = currentDate;
